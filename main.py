f7d1805269172bc34b660ed4d0c03917c402435d
Ajhesh Basnet <=>
2025-09-18 22:36:31 +0530

    second commit

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import os
 
 df = pd.read_csv(r'c:\\Users\\hp\\Downloads\\dataset\\crocodile_dataset.csv')
 df = df.iloc[:100, :5]
-print(df)
 
+data_dir= "data"
 
-if os.
+os.makedirs(data_dir, exist_ok=True)
+
+# Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# Save the DataFrame to a CSV file, including column names
+df.to_csv(file_path, index=False)
+
+print(f"CSV file saved to {file_path}")
